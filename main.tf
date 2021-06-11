9070f85b549a75e8ba9639cd6d9a81af71876e9b
Holger Justen <holger.justen@aktion-mensch.de>
2021-06-11 11:07:45 +0200

    Add tags

--- main.tf
+++ main.tf
@@ -37,6 +37,13 @@ resource "random_integer" "app_service_name_suffix" {
 resource "azurerm_resource_group" "spacegame" {
   name     = var.resource_group_name
   location = var.resource_group_location
+  tags = {
+    environment   = "test"
+    contact       = "Holger Justen"
+    kostenstelle  = "340006"
+    kostentraeger = "3420AZUREMGMT"
+    application   = "common"
+  }
 }
 
 resource "azurerm_app_service_plan" "spacegame" {
@@ -50,6 +57,9 @@ resource "azurerm_app_service_plan" "spacegame" {
     tier = "Basic"
     size = "B1"
   }
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
 resource "azurerm_app_service" "spacegame_dev" {
@@ -61,6 +71,9 @@ resource "azurerm_app_service" "spacegame_dev" {
   site_config {
     linux_fx_version = "DOTNETCORE|3.1"
     app_command_line = "dotnet Tailspin.SpaceGame.Web.dll"
+  }
+  lifecycle {
+    ignore_changes = [tags]
   }
 }
 
